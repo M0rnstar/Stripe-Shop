84022bc40bf7ef96722dad21c1931790105575ef
M0rnstar <azamat.mambetov.2014@gmail.com>
2025-02-25 20:20:48 +0300

    Добавлена модель Item

--- shop_stripe/shop/models.py
+++ shop_stripe/shop/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название товара")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание товара")
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Цена товара")
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+        ordering = ['name']
