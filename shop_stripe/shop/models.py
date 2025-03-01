6eb146082a80cb2ce93de1a631ab9032353b4e84
M0rnstar <azamat.mambetov.2014@gmail.com>
2025-03-01 18:38:53 +0300

    Добавлен выбор валюты

--- shop_stripe/shop/models.py
+++ shop_stripe/shop/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 
 class Item(models.Model):
+    CURRENCY_CHOISES = (
+        ('USD', 'Доллар'),
+        ('RUB', 'Рубль')
+    )
+
     name = models.CharField(max_length=255, verbose_name="Название товара")
     description = models.TextField(default="", verbose_name="Описание товара")
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена товара")
-    currency = models.CharField(max_length=10, default="USD", verbose_name="Валюта")
+    price = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2, 
+        verbose_name="Цена товара")
+    currency = models.CharField(
+        max_length=10, 
+        choices=CURRENCY_CHOISES,
+        default="USD", 
+        verbose_name="Валюта"
+        )
 
     class Meta:
         verbose_name = "Товар"
