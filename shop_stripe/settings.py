f9e66ef0e97eb1d41727c5b6e6a71399cbef9192
M0rnstar <azamat.mambetov.2014@gmail.com>
2025-03-03 22:58:49 +0300

    #

--- shop_stripe/settings.py
+++ shop_stripe/settings.py
@@ -36,7 +36,7 @@ DEBUG = os.environ.get("DEBUG", "True") == "True"
 if DEBUG:
     ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 else:
-    ALLOWED_HOSTS = os.environ.get("DEBUG", "True").split(",")
+    ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 
 
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
